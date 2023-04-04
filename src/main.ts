23b2157169e528f5328fbff5efb2bcea21a946c7
Vasil Genov <vasil.genov@psinteractive.io>
2023-04-04 22:31:08 +0300

    prettier update

--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,46 @@
-import * as THREE from "three";
-import "./style.css";
-
-// Show
-document.body.style.display = "block";
+import * as THREE from "three"
+import "./style.css"
 
 // Sizes
-const useWindowDimensions = () => [window.innerWidth, window.innerHeight];
-let [w, h] = useWindowDimensions();
+const useWindowDimensions = () => [window.innerWidth, window.innerHeight]
+let [w, h] = useWindowDimensions()
 
 // Scene
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
-camera.position.z = 5;
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000)
+camera.position.z = 5
 
 // Renderer
-const canvas = document.querySelector(".webgl") as HTMLCanvasElement;
-const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize(w, h);
+const canvas = document.querySelector(".webgl") as HTMLCanvasElement
+const renderer = new THREE.WebGLRenderer({ canvas })
+renderer.setSize(w, h)
 
 // Resize
 window.addEventListener("resize", () => {
-  [w, h] = useWindowDimensions();
-  camera.aspect = w / h;
-  camera.updateProjectionMatrix();
-  renderer.setSize(w, h);
-});
+  ;[w, h] = useWindowDimensions()
+  camera.aspect = w / h
+  camera.updateProjectionMatrix()
+  renderer.setSize(w, h)
+})
 
 // View
 const cube = new THREE.Mesh(
   new THREE.BoxGeometry(1, 1, 1),
   new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-);
-scene.add(cube);
+)
+scene.add(cube)
 
 // Animate
 function animate() {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  cube.rotation.x += 0.01
+  cube.rotation.y += 0.01
 }
 
 // Loop
 function loop() {
-  animate();
-  renderer.render(scene, camera);
-  window.requestAnimationFrame(loop);
+  animate()
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(loop)
 }
 
-loop();
+loop()
